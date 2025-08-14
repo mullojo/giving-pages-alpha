fc5e43cb1ff0ce94bb84b00f36de97a64c21ded4
Bob <rpmullaly@gmail.com>
2025-08-13 23:34:40 -0700

    Revert webkit tweak

--- scripts.js
+++ scripts.js
@@ -125,18 +125,8 @@ const app = {
                         let type, image;
 
                         if (isWebKit) {
-                            //type = "canvas";
-                            //image = false;
-
-                            type = "svg";
-                            const icon = await loadIcon(service.icon);
-                            const css = getIconCSS(icon, { color: "white" });
-                            const regex =
-                                /background-image:\s*url\(['"](data:image\/svg\+xml,.*?)['"]\);/;
-                            const match = css.match(regex);
-                            const dataImageUrl = match[1].trim();
-                            image = dataImageUrl;
-
+                            type = "canvas";
+                            image = false;
 
                         } else {
                             type = "svg";
